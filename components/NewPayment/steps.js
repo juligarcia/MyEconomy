820f4f7d73f2a670eaa7ec0f9ce85e42fbd8fb0a
Julian Garcia <juli.garcia.77@gmail.com>
2020-10-04 17:31:03 -0300

    fix: removed unused component

--- components/NewPayment/steps.js
+++ components/NewPayment/steps.js
@@ -1,5 +1,4 @@
 import BasicPaymentInfo from './components/BasicPaymentInfo';
-import Submit from './components/Submit';
 import TotalAndInstalments from './components/TotalAndInstalments';
 
 const Steps = [BasicPaymentInfo, TotalAndInstalments];
